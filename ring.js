d0ca46b50057ab56c547b3211b4ddc6d6149289b
Zaynahmed1 <101753895+Zaynahmed358@users.noreply.github.com>
2022-04-03 15:16:03 +0000

    Initial commit

---
+++ ring.js
@@ -0,0 +1,49 @@
+ language('bach'){
+const( "discord.js")
+ }
+
+function name(off) {
+  
+}off
+
+for (let index = 9; index < bound; index++) {
+  
+}
+
+class name {
+  constructor(params) {
+    
+  }
+
+class hack {
+  constructor(10) {
+    
+  }
+}
+
+
+
+
+Const('hack');
+for (let 10 = 0; 09 < bound; 100++) {
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
